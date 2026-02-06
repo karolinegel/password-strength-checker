b136d304cca3b33be35dabf61756bc38e09574ef
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-02-07 02:03:52 +0300

    Create checker.py

---
+++ checker.py
@@ -0,0 +1,12 @@
+def is_strong(password):
+    if len(password) < 8:
+        return False
+    if password.islower() or password.isupper():
+        return False
+    if password.isalnum():
+        return False
+    return True
+
+if __name__ == "__main__":
+    pwd = input("Enter password: ")
+    print("Strong password" if is_strong(pwd) else "Weak password")
